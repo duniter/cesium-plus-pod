0ed6f14bdec31f0cd3de5a106e595af110de5928
blavenie <benoit.lavenier@e-is.pro>
2020-01-25 11:52:13 +0100

    [enh] Release script: add an automatic rollback if need

--- release.sh
+++ release.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
+mkdir -p .local
+
+RELEASE_OPTS="-DskipTests -DperformFullRelease"
+
+# Rollback previous release, if need
+if [[ -f "pom.xml.releaseBackup" ]]; then
+    echo "**********************************"
+    echo "* Rollback previous release..."
+    echo "**********************************"
+    result=`mvn release:rollback`
+    failure=`echo "$result" | grep -m1 -P "\[INFO\] BUILD FAILURE"  | grep -oP "BUILD \w+"`
+    # rollback failed
+    if [[ ! "_$failure" = "_" ]]; then
+        echo "$result" | grep -P "\[ERROR\] "
+        exit 1
+    fi
+    echo "Rollback previous release [OK]"
+fi
 
 echo "**********************************"
 echo "* Preparing release..."
 echo "**********************************"
-result=`mvn release:clean`
-failure=`echo "$result" | grep -m1 -P "\[INFO\] BUILD FAILURE"  | grep -oP "BUILD \w+"`
-# prepare failed
-if [[ ! "_$failure" = "_" ]]; then
-    echo "$result" | grep -P "\[ERROR\] "
-    exit 1
-fi
-
-mvn release:prepare -Darguments="-DskipTests -DperformFullRelease"
+mvn release:prepare --quiet -Darguments="${RELEASE_OPTS}"
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
+echo "Prepare release [OK]"
 
 echo "**********************************"
 echo "* Performing release..."
 echo "**********************************"
-mvn release:perform --quiet -Darguments="-DskipTests -DperformFullRelease"
+mvn release:perform --quiet -Darguments="-DskipTests -Denv=hsqldb"
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
+echo "Perform release [OK]"
 
 echo "**********************************"
 echo "* Uploading artifacts to Github..."
